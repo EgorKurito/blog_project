7b6db14c5599fca4744959860c4f428fa670b628
Egor <ekurito@gmail.com>
2019-01-27 12:09:26 +0300

    add postgreSQL

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'egor',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5444',
     }
 }
 
